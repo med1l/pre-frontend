96eadfe156eee75f5ea5b06b4ebe286110c962de
med1l <medl@bk.ru>
2018-09-20 00:52:58 +0300

    HomeWork 5 (Not All)

---
+++ Homework5/js/homework5.js
@@ -0,0 +1,5 @@
+function goToPage()
+{
+	var url = document.getElementById('http://www.mail.ru');
+	document.location.href = url.value;
+}
---
+++ Homework5/js/js.js
@@ -0,0 +1,12 @@
+function goToPage()
+{
+	document.location.href = "index.html";
+}
+function style () {
+	document.body.style.background = 'none';
+	document.getElementsByTagName('div')[0].style.backgroundColor = 'red';
+	document.getElementsByTagName('div')[0].style.border = '5px solid black';
+	document.getElementById('content').style.borderRadius = '50%';
+	document.getElementsByTagName('a')[1].style.fontSize = '40px';
+	document.getElementsByTagName('a')[1].style.color = 'black';
+}
