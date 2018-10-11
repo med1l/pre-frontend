ef8030da2044ccfc203a14c844db3ce4ad1b3c7c
med1l <medl@bk.ru>
2018-10-12 00:50:48 +0300

    HomeWork 5

---
+++ Homework6/js/js6.js
@@ -0,0 +1,29 @@
+function goToPage()
+{
+	document.location.href = "index.html";
+}
+function style () {
+	document.body.style.background = 'none';
+	document.getElementsByTagName('div')[0].style.backgroundColor = 'red';
+	document.getElementsByTagName('div')[0].style.border = '5px solid black';
+	document.getElementById('content').style.borderRadius = '50%';
+	document.getElementsByTagName('a')[1].style.fontSize = '40px';
+	document.getElementsByTagName('a')[1].style.color = 'black';
+	document.getElementsByTagName('a')[0].style.fontSize = '15px';
+	document.getElementsByTagName('a')[0].style.color = 'black';
+}
+function clearBox()
+{
+    document.getElementsByTagName("div")[0].innerHTML = '';
+    document.getElementsByTagName("div")[0].innerHTML += '<div id="box1"> \
+                                							<div id="box2"> \
+                                    							<div id="box3"> \
+                                    							 <a href="homework5.html">BACk</a>\
+                                    							</div> \
+                                							</div> \
+                            							</div>';
+}
+function toggleSidebar(ref) {
+	ref.classList.toggle('active');
+	document.getElementById('sidebar').classList.toggle('active');
+}
