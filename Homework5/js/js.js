6591155059fb44954d9296b0e73b6d4992b3ab1d
med1l <medl@bk.ru>
2018-10-12 00:44:09 +0300

    HomeWork 5

--- Homework5/js/js.js
+++ Homework5/js/js.js
@@ -23,3 +23,7 @@ function clearBox()
                                 							</div> \
                             							</div>';
 }
+function toggleSidebar(ref) {
+	ref.classList.toggle('active');
+	document.getElementById('sidebar').classList.toggle('active');
+}
